0948abd105fb0f3037c22efd474a1b693e46793f
Ibai <ibai.mansoai@elorrieta-errekamari.com>
2026-05-21 00:15:08 +0200

    Bug fix

--- app.js
+++ app.js
@@ -6,9 +6,9 @@
 const DATA_SRC = 'https://raw.githubusercontent.com/openfootball/worldcup.json/refs/heads/master/2026';
 // EDITA ESTAS 3 COSAS POR FAVOR
 // POR FAVOR
-const LEADERBOARD_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSDbPhOej3DnN_bdvrCQ5R0T6HZg6bBaxKdH17J_Pc3oGOkKkd9V83BUDYlBSCevOrqYK2XQuA7ZMCx/pub?gid=1633860364&single=true&output=csv'
-const FORM_ID = '1FAIpQLSdiF0qsK65DcaadNKRzDbue8xtkzAIIev-7yqUqAH3srhEAQg';
-const ENTRY_ID = 'entry.496944209';
+const LEADERBOARD_CSV_URL = 'https://docs.google.com/spreadsheets/d/19y97W2oH_e_P5wlMdg1rd03DPqfx35kUHNCUHExHSPQ/edit?usp=sharing';
+const FORM_ID = '1luEVDniA8KPGMluLJP24sT7eBEMpCgA6sKTPBYpFTR8';
+const ENTRY_ID = 'entry.1314337550';
 // REPITO, POR FAVOR
 
 // Puedes cambiar los valores por tus propias puntuaciones si quieres
